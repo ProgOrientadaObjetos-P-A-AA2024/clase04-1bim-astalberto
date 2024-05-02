4d43a225fc20d19800bc3716c7d22068e7ec2110
astalberto <albertoherrera205@hotmail.com>
2024-05-02 16:13:24 -0500

    Push

--- ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete7/FacturaTelefonica.java
+++ ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete7/FacturaTelefonica.java
@@ -60,5 +60,15 @@ public class FacturaTelefonica {
     public double obtenerValorFactura() {
         return valorFactura;
     }
-
+    
+    @Override
+    public String toString(){
+        String mensaje = String.format("Número telefónico: %s\n"
+                + "Minutos mes: %.2f\nValor minuto: %.2f\n"
+                + "Valor factura: %.2f\n", obtenerNumeroTelefono(),
+                obtenerMinutosMes(),
+                obtenerValorMinuto(),
+                obtenerValorFactura());
+        return mensaje; 
+    }
 }
