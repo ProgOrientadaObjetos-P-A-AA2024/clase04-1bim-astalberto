db0071066b29f9e554c40649c1f2851e50f8ab37
astalberto <albertoherrera205@hotmail.com>
2024-05-02 06:16:18 -0500

    push

--- ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete2/Ejecutor.java
@@ -12,20 +12,51 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejecutor {
+
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
+        Scanner entrada = new Scanner(System.in);
         /*Crear un objeto y presentar los datos
         Ingresar la información por teclado.
-        */
-        
-        Hospital h1 = new Hospital("Militar", "Loja", 1000, 3000);
-        
+         */
+        String ingresoNombre = "", ingresoCiudad = "";
+        int ingresoDoctores = 0, ingresoEnfermos = 0;
+
+        System.out.println("Ingrese nombre");
+        ingresoNombre = entrada.nextLine();
+        System.out.println("Ingrese Ciudad");
+        ingresoCiudad = entrada.nextLine();
+        System.out.println("Ingrese numero de Doctores");
+        ingresoDoctores = entrada.nextInt();
+        System.out.println("Ingrese numero de Enfermos");
+        ingresoEnfermos = entrada.nextInt();
+        entrada.nextLine();
+
+        Hospital h1 = new Hospital(ingresoNombre, ingresoCiudad,
+                ingresoDoctores, ingresoEnfermos);
+
         h1.establecerNombre("Hospital Militar Privado");
         
-        Hospital h2 = new Hospital("Militar DOS", "Loja", 1000, 3000);
+        System.out.println("Ingrese nombre");
+        ingresoNombre = entrada.nextLine();
+        System.out.println("Ingrese Ciudad");
+        ingresoCiudad = entrada.nextLine();
+        System.out.println("Ingrese numero de Doctores");
+        ingresoDoctores = entrada.nextInt();
+        System.out.println("Ingrese numero de Enfermos");
+        ingresoEnfermos = entrada.nextInt();
         
-        System.out.printf("%s\n", h1);
-        System.out.printf("%s\n", h2);
-        
+        Hospital h2 = new Hospital(ingresoNombre, ingresoCiudad,
+                ingresoDoctores, ingresoEnfermos);
+        System.out.println("Hospital 1");
+        System.out.printf("%s\n%s\n%d\n%d\n", h1.obtenerNombre(),
+                h1.obtenerCiudad(),
+                h1.obtenerNumeroDoctores(),
+                h1.obtenerNumeroEnfermeros());
+                System.out.println("Hospital 2");
+        System.out.printf("%s\n%s\n%d\n%d\n", h2.obtenerNombre(),
+                h2.obtenerCiudad(),
+                h2.obtenerNumeroDoctores(),
+                h2.obtenerNumeroEnfermeros());
+
     }
 }
